7ee3d0a9a68d15cdff2a626c64a1893ba9c904e7
denisovdaniil <denisovdaniil95@gmail.com>
2025-01-04 21:55:38 +0100

    Add files via upload

--- 04/04_file.py
+++ 04/04_file.py
@@ -1,1 +1,10 @@
+# 04_file.py
 
+hodnota = input('Zadejte hodnotu: ')
+
+def append():
+    
+    with open('Py-ukoly/04/04_file.txt', mode = 'a', encoding = 'utf-8') as file:
+        file.write(hodnota + "\n")
+
+append()
